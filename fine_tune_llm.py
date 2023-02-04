4b4c0a7c7afdb5d5e9227aab929bd5629f9a2d07
Brian Spiering <bspiering>
2023-02-04 10:01:30 -0800

    Add example fine tuning on local data

---
+++ fine_tune_llm.py
@@ -0,0 +1,49 @@
+"""
+Fine tune large language model (LLM) on local data.
+"""
+
+from datasets     import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, DataCollatorForLanguageModeling, pipeline, TrainingArguments, Trainer
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"],
+                    padding="max_length",
+                    truncation=True)
+
+
+dataset = load_dataset(path="data/", data_files="input.txt")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+small_train_dataset = tokenized_dataset["train"].shuffle(seed=42).select(range(1000))
+model = AutoModelForCausalLM.from_pretrained("distilroberta-base")
+
+training_args = TrainingArguments(
+    output_dir="test_trainer",
+    evaluation_strategy = "epoch",
+    learning_rate=2e-5,
+    weight_decay=0.01,
+)
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, 
+    mlm_probability=0.15
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=small_train_dataset,
+    eval_dataset=small_train_dataset,
+    data_collator=data_collator,
+)
+
+trainer.train()
+
+pipe = pipeline(
+    "text-generation", 
+    model=model,
+    tokenizer=tokenizer
+)
+
+prompt = "What color are pandas?"
+print(pipe(prompt, num_return_sequences=1, max_new_tokens=3)[0]["generated_text"])
